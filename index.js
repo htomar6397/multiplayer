be0bc874bf144086338235151602d5f3005d0445
htomar6397 <126243160+htomar6397@users.noreply.github.com>
2023-04-25 09:53:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require("http").Server(app);
 const PORT = 4000;
 const socketIO = require("socket.io")(http, {
   cors: {
-    origin: "https://tic-tac-toe-qwerty.000webhostapp.com",
+    origin: "http://tic-tac-toe-qwerty.000webhostapp.com",
   },
 });
 
