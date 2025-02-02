e082431de1119131592136fd6642c6c385a99581
Mayank Tomar <126243160+htomar6397@users.noreply.github.com>
2025-02-02 21:02:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.get("/api/sections", async (req, res) => {
   }
 });
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 
 // Serve React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
